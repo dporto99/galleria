6f48aad55e5aaf5134d16a43d9bbdcce966478cb
David Porto <dporto99@gmail.com>
2012-06-01 15:44:10 -0400

    Changes before template creation and integration.

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,16 @@
 $(document).ready(function(){
-  $.Resize.initialize();
   $.Position.initialize();
 
   $(window).resize(function(){
-    $.Resize.initialize();
+    $.Position.initialize();
   })
 })
 
-$.Resize = {
-  initialize: function(){
-    this.height();
-    this.width();
-  },
-
-  height: function() {
-    var height = ($(window).height() - $('#inner').height())/2;
-    if (height > 0)
-    $('#outer').css('margin-top',height);
-  },
-
-  width: function() {
-    var width = ($(window).width() - $('#inner').width())/2;
-    if (width > 0)
-    $('#outer').css('margin-left',width);
-  }
-}
-
 $.Position = {
-  initialize: function() {
-    this.position();
-  },
-
-  position: function() {
-    if ($('#content').height() < $('.menu').height()){
-      $('.menu').css('position','static');
-      return;
+  initialize: function(){
+    var height = ($(window).height() - $('#inner').height())/2;
+    if (height > 0){
+      $('#inner').css('top',(height-5));
     }
-    var left = $('#content').offset().left;
-    var top = $('#content').offset().top;
-    $('.menu').css('left', left + 720 + 'px');
-    $('.menu').css('top', top + 36 + 'px');
   }
 }
